df29fc7ff66521903e29fff716c0457386caf17c
Andrew Stewart <andrew.stewart@i-nnovate.net>
2014-12-12 21:29:56 +0000

    Added Sulfura tests

---
+++ GuildedRose.UnitTests/GivenSeveralItems.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GuildedRose.UnitTests
+{
+	public class GivenSeveralItems {
+
+		private List<Item> items = new List<Item>();
+
+		public GivenSeveralItems () 
+		{
+			items.Add(new Item("NORMAL ITEM", 5, 10));
+			items.Add(new Item("Aged Brie", 3, 10));
+		}
+
+		[Fact]
+		public void test() {
+			GildedRose.updateQuality(items);
+
+			Assert.Equal(9, items[0].quality);
+			Assert.Equal(4, items[0].sellIn);
+			Assert.Equal(11, items[1].quality);
+			Assert.Equal(2, items[1].sellIn);
+		}
+	}
+}
---
+++ GuildedRose.UnitTests/GivenSulfuras.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GuildedRose.UnitTests
+{
+	public class GivenSulfuras {
+
+		static String SULFURAS = "Sulfuras, Hand of Ragnaros";
+		
+		private Item updateFrom(int initialSellIn, int initialQuality) {
+			Item item = new Item(SULFURAS, initialSellIn, initialQuality);
+			List<Item> items = new List<Item>();
+			items.Add(item);
+			GildedRose.updateQuality(items);
+			return item;
+		}
+
+		[Fact]
+		public void beforeSellDate() {
+			Item item = updateFrom(5, 80);
+			Assert.True(item.quality == 80);
+			Assert.True(item.sellIn == 5);
+		}
+
+		[Fact]
+		public void onSellDate() {
+			Item item = updateFrom(0, 80);
+			Assert.True(item.quality == 80);
+			Assert.True(item.sellIn == 0);
+		}
+
+		[Fact]
+		public void afterSellDate() {
+			Item item = updateFrom(-10, 80);
+			Assert.True(item.quality == 80);
+			Assert.True(item.sellIn == -10);
+		}
+	}
+}
