cf50bf75cb0ca881c3c2a66777c7dab4e15b0dac
Hemant Kumar <hemantksingh.hk@gmail.com>
2014-12-19 15:17:11 +0000

    Repalced switch statement by using object composition.

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -1,72 +1,126 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace GildedRose
 {
     public class GildedRose
     {
+        private static readonly Dictionary<string, IProduct> Products = new Dictionary<string, IProduct>
+            {
+                {"Aged Brie", new Product(new AgedBrieQualityAdjuster(2))},
+                {"Backstage passes to a TAFKAL80ETC concert", new Product(new BackstageQualityAdjuster(1))},
+                {"Sulfuras, Hand of Ragnaros", new SulfurasProduct()},
+                {"NORMAL ITEM", new Product(new NormalQualityAdjuster(-1))},
+                {"Conjured Mana Cake", new Product(new ConjuredQualityAdjuster(0))}
+            };
+
         public static void UpdateQuality(List<Item> items)
         {
-            for (int i = 0; i < items.Count(); i++)
-            {
-                Item item = items[i];
+            foreach (Item item in items)
+                Products[item.Name].Update(item);
+        }
+    }
 
-                int qualityAdjustment = 0;
-                switch (item.Name)
-                {
-                    case "Aged Brie":
-                        //new Product().Update(item);
-                        item.SellIn = item.SellIn - 1;
-                        qualityAdjustment = item.SellIn < 0 ? +2 : +1;
-                        item.Quality = item.Quality + qualityAdjustment;
+    internal class SulfurasProduct : IProduct
+    {
+        public void Update(Item item)
+        {
+        }
+    }
 
-                        item.Quality = Math.Min(50, item.Quality);
-                        item.Quality = Math.Max(0, item.Quality);
-                        break;
-                    case "Backstage passes to a TAFKAL80ETC concert":
-                        item.SellIn = item.SellIn - 1;
+    public class ConjuredQualityAdjuster : IAdjustQuality
+    {
+        private readonly int _defaultQuality;
 
-                        if (item.SellIn < 0)
-                        {
-                            qualityAdjustment = -item.Quality;
-                        }
+        public ConjuredQualityAdjuster(int defaultQuality)
+        {
+            _defaultQuality = defaultQuality;
+        }
 
-                        else if (item.SellIn < 5)
-                        {
-                            qualityAdjustment = +3;
-                        }
-                        else if (item.SellIn < 10)
-                        {
-                            qualityAdjustment = +2;
-                        }
-                        else
-                        {
-                            qualityAdjustment = +1;
-                        }
-                        item.Quality = item.Quality + qualityAdjustment;
+        public int Adjust(Item item)
+        {
+            return _defaultQuality;
+        }
+    }
 
-                        item.Quality = Math.Min(50, item.Quality);
-                        item.Quality = Math.Max(0, item.Quality);
-                        break;
-                    case "Sulfuras, Hand of Ragnaros":
-                        break;
+    public class NormalQualityAdjuster : IAdjustQuality
+    {
+        private readonly int _defaultQuality;
 
-                    case "NORMAL ITEM":
-                        item.SellIn = item.SellIn - 1;
+        public NormalQualityAdjuster(int defaultQuality)
+        {
+            _defaultQuality = defaultQuality;
+        }
 
-                        if (item.SellIn < 0)
-                            qualityAdjustment = -1;
+        public int Adjust(Item item)
+        {
+            return item.SellIn < 0 ? -2 : _defaultQuality;
+        }
+    }
 
-                        item.Quality = item.Quality + qualityAdjustment;
-                        item.Quality = Math.Min(50, item.Quality);
-                        item.Quality = Math.Max(0, item.Quality - 1);
-                        break;
-                    case "Conjured Mana Cake":
-                        item.SellIn = item.SellIn - 1;
-                        break;
-                }
-            }
+    public class BackstageQualityAdjuster : IAdjustQuality
+    {
+        private readonly int _defaultQuality;
+
+        public BackstageQualityAdjuster(int defaultQuality)
+        {
+            _defaultQuality = defaultQuality;
+        }
+
+        public int Adjust(Item item)
+        {
+            if (item.SellIn < 0)
+                return -item.Quality;
+            if (item.SellIn < 5)
+                return +3;
+            if (item.SellIn < 10)
+                return +2;
+
+            return _defaultQuality;
+        }
+    }
+
+    public interface IAdjustQuality
+    {
+        int Adjust(Item item);
+    }
+
+    public class AgedBrieQualityAdjuster : IAdjustQuality
+    {
+        private readonly int _qualityAdjuster;
+
+        public AgedBrieQualityAdjuster(int qualityAdjuster)
+        {
+            _qualityAdjuster = qualityAdjuster;
+        }
+
+        public int Adjust(Item item)
+        {
+            return item.SellIn < 0 ? _qualityAdjuster : +1;
+        }
+    }
+
+    public interface IProduct
+    {
+        void Update(Item item);
+    }
+
+    public class Product : IProduct
+    {
+        private readonly IAdjustQuality _qualityAdjuster;
+
+        public Product(IAdjustQuality qualityAdjuster)
+        {
+            _qualityAdjuster = qualityAdjuster;
+        }
+
+        public void Update(Item item)
+        {
+            item.SellIn = item.SellIn - 1;
+            item.Quality = item.Quality + (_qualityAdjuster.Adjust(item));
+
+            item.Quality = Math.Min(50, item.Quality);
+            item.Quality = Math.Max(0, item.Quality);
         }
     }
 }
